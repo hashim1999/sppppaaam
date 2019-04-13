c8473abcad8e20cddb029ea955526f55f70130ee
hashim1999 <48349004+hashim1999@users.noreply.github.com>
2019-04-13 22:43:23 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("400195036249325568")
+setInterval(function() {
+channel.send(`اححححححححبكككككممممم`);
+}, 30)
+})
+
+client.login(NDY3MTkzMDU3NjEyMzk4NTkz.XKuQ_w.BdPBrrwo002Ni6izznsW1FN5tgs);
